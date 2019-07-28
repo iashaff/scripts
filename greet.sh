46bf990ca7c480f39098453287e937edf3eedfb4
Irina Shalgina <optimistka30@mail.ru>
2019-07-28 22:46:15 +0700

    added greeting script

---
+++ greet.sh
@@ -0,0 +1,18 @@
+a="Доброе утро!"
+b="Добрый день!"
+c="Добрый вечер!"
+d="Доброй ночи!"
+x=`date +%H`
+
+if [ $x -le 5 ]; then
+	echo "$d `date -R`"
+elif [ $x -le 12 ]; then
+        echo "$a `date -R`"
+elif [ $x -le 17 ]; then
+        echo "$b `date -R`"
+elif [ $x -le 22 ]; then
+        echo "$c `date -R`"
+else
+        echo "$d `date -R`"
+fi       
+
